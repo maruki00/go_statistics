b51ac70a7cb6eedf4ced820825135ba659936d69
Abdellah Oulahyane <aitbaha48@gmail.com>
2024-03-25 18:45:50 +0300

    --

--- pkg/DB/DB.go
+++ pkg/DB/DB.go
@@ -1,1 +1,23 @@
 package db
+
+import (
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type DB struct {
+	db *gorm.DB
+}
+
+func (obj *DB) GetDB() *gorm.DB {
+	dsn := "root:user@tcp(127.0.0.1:3306)/go_appstatics?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("DB Error")
+	}
+
+	obj.db = db
+	return obj.db
+}
